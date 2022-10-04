9b71f5b6514456829d2bbbea28f7b9b5009a160e
Student <student@iMac-Astrum-61.local>
2022-10-04 18:09:53 +0500

    Calc

--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import  "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function App() {
+  /* eslint no-eval: 0 */
+
+  const [value, setValue] = useState("");
+  const calcButtons = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
+    calcButtons.push(
+      <button
+        onClick={(e) => {
+          setValue(value + e.target.value);
+        }}
+        value={item}
+        key={item}
+      >
+        {" "}
+        {item}
+      </button>
+    );
+  });
+
+  return (
+    <div className="wrapper">
+      {" "}
+      <div className="show-wrapper">
+      <div className="show-input">
+        <div className="input-center">
+          {value}
+        </div>
+        </div>
+      </div>
+      <div className="btnWrapper">
+      <div className="btns">{calcButtons}</div>
+        <div className="btnMath">
+        <div className="modifiers subgrid">
+        {/* clear button */}
+
+        <button className="mathbtn"onClick={() => setValue(value.substr(0, value.length - 1))}>
+          Clear
+        </button>
+
+        {/* clear all */}
+        <button className="mathbtn"onClick={() => setValue("")} value="">
+          AC
+        </button>
+      </div>
+      <div className="operations subgrid">
+        {/* add button */}
+        <button className="mathbtn"onClick={(e) => setValue(value + e.target.value)} value="+">
+          +
+        </button>
+
+        {/* minus btn */}
+        <button className="mathbtn"onClick={(e) => setValue(value + e.target.value)} value="-">
+          {" "}
+          -{" "}
+        </button>
+
+        <button className="mathbtn"onClick={(e) => setValue(value + e.target.value)} value="*">
+          {" "}
+          *
+        </button>
+
+        <button className="mathbtn"onClick={(e) => setValue(value + e.target.value)} value="/">
+          {" "}
+          /
+        </button>
+        <button
+          onClick={(e) => {
+            try {
+              setValue(
+                String(eval(value)).length > 3 &&
+                  String(eval(value)).includes(".")
+                  ? String(eval(value).toFixed(4))
+                  : String(eval(value))
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          value="="
+        >
+          =
+        </button>
+      </div>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
